fix in neural net in training test, backprop seems ok

tests/test_training.py
@@ -12,23 +12,25 @@ DATA_FOLDER = os.path.join(os.path.dirname(__file__), "fixtures", "training-fixt
 EPOCHS = 10
 torch.manual_seed(0)
 
+
 class NeuralNetwork(nn.Module):
     def __init__(self, input_shape):
         super().__init__()
-        self.net =  torch.nn.Sequential(
-                    torch.nn.Linear(input_shape, 10),
-                    torch.nn.Sigmoid(),
-                    torch.nn.Linear(10, input_shape),
-                )
+        self.net = torch.nn.Sequential(
+            torch.nn.Linear(input_shape, 10),
+            torch.nn.Sigmoid(),
+            torch.nn.Linear(10, input_shape),
+        )
         torch.nn.init.normal_(self.net[0].weight, mean=0.0, std=1.0)
         torch.nn.init.normal_(self.net[2].weight, mean=0.0, std=1.0)
+
     def forward(self, x):
         x = self.net(x)
         return x
 
+
 def _setup_network(input_shape: int) -> torch.nn.Module:
     net = NeuralNetwork(input_shape)
-
     return net
 
 
@@ -48,12 +50,9 @@ def test_training():
     loader = _setup_data(DATA_FOLDER)
     input_size = next(iter(loader))[0].shape[-1]
     net = _setup_network(input_size).to(device)
-    optimizer = Adam(net.parameters(), lr=1e-5)
+    optimizer = Adam(net.parameters(), lr=1e-2)
+    before_params = torch.cat([p.view(1, -1) for p in net.parameters()], dim=-1)
     for _ in range(EPOCHS):
-        previous_params = []
-        for param in net.parameters():
-            previous_params.append(param.clone())
-        second_layer = list(net.parameters())[2]
         for input, ref in iter(loader):
             input, ref = input.to(device), ref.to(device)
             output = net(input)
@@ -61,8 +60,8 @@ def test_training():
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-        after_params = []
-        for param in net.parameters():
-            after_params.append(param.clone())
-    for i in range(len(after_params)):
-        assert not torch.allclose(after_params[0], previous_params[0])
+            grads = torch.cat([p.grad.view(1, -1) for p in net.parameters()], dim=-1)
+            assert torch.linalg.vector_norm(grads) > 1e-5
+            assert not torch.allclose(grads, torch.zeros(grads.shape))
+    after_params = torch.cat([p.view(1, -1) for p in net.parameters()], dim=-1)
+    assert not torch.allclose(before_params, after_params)