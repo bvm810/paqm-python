34b815ded7b5b9ae042fac8756d29eb404ef4662
abreuwallace <abreu.encgb@poli.ufrj.br>
2023-10-30 22:51:30 -0300

    PAQM as loss function test

--- tests/test_training.py
+++ tests/test_training.py
@@ -1,5 +1,6 @@
 import os
 import torch
+import torch.nn as nn
 from torch.utils.data import DataLoader
 from torch.optim import SGD, Adam
 from typing import Tuple, List
@@ -7,18 +8,27 @@ from paqm.paqm import PAQM
 from paqm.utils import collate
 from paqm.utils.dataset import PAQMDataset
 
-DATA_FOLDER = os.path.join(os.path.dirname(__file__), "fixtures", "training-fixtures")
+DATA_FOLDER = os.path.join('/nfs/home/wallace.abreu/Mestrado/paqm-python/tests', "fixtures", "training-fixtures")
 EPOCHS = 10
+torch.manual_seed(0)
 
+class NeuralNetwork(nn.Module):
+    def __init__(self, input_shape):
+        super().__init__()
+        self.net =  torch.nn.Sequential(
+                    torch.nn.Linear(input_shape, 10),
+                    torch.nn.Sigmoid(),
+                    torch.nn.Linear(10, input_shape),
+                )
+        torch.nn.init.normal_(self.net[0].weight, mean=0.0, std=1.0)
+        torch.nn.init.normal_(self.net[2].weight, mean=0.0, std=1.0)
+    def forward(self, x):
+        x = self.net(x)
+        return x
 
 def _setup_network(input_shape: int) -> torch.nn.Module:
-    net = torch.nn.Sequential(
-        torch.nn.Linear(input_shape, 10),
-        torch.nn.Sigmoid(),
-        torch.nn.Linear(10, input_shape),
-    )
-    torch.nn.init.normal_(net[0].weight, mean=0.0, std=1.0)
-    torch.nn.init.normal_(net[2].weight, mean=0.0, std=1.0)
+    net = NeuralNetwork(input_shape)
+
     return net
 
 
@@ -40,15 +50,19 @@ def test_training():
     net = _setup_network(input_size).to(device)
     optimizer = Adam(net.parameters(), lr=1e-5)
     for _ in range(EPOCHS):
-        first_layer = net[0].weight
-        second_layer = net[2].weight
+        previous_params = []
+        for param in net.parameters():
+            previous_params.append(param.clone())
+        second_layer = list(net.parameters())[2]
         for input, ref in iter(loader):
             input, ref = input.to(device), ref.to(device)
             output = net(input)
-            # loss = PAQM(output, ref).score.mean()
-            loss = torch.nn.functional.mse_loss(output, ref)
+            loss = PAQM(output, ref).score.mean()
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-        assert not torch.allclose(first_layer, net[0].weight)
-        assert not torch.allclose(second_layer, net[2].weight)
+        after_params = []
+        for param in net.parameters():
+            after_params.append(param.clone())
+    for i in range(len(after_params)):
+        assert not torch.allclose(after_params[0], previous_params[0])
